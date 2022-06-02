d6dafbbc76e2990036812be226d0fea5ffaaeb4d
Stefan Angerer <angerer.st@gmail.com>
2022-06-02 17:29:51 +0200

    Popup import

--- main.js
+++ main.js
@@ -72,15 +72,42 @@ async function loadWind(url) {
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
+
+layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no"); 
+
+let marker = L.circleMarker([
+    47.267222, 11.392778
+]).bindPopup("Wettervorhersage").addTo(overlays.weather);
+
+
 // Wettervorhersage
 async function loadWeather(url) {
     const response = await fetch(url); 
     const jsondata = await response.json();
     
-    layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no"); 
+    //Marker positionieren 
+    marker.setLatLng([
+        jsondata.geometry.coordinates[1], 
+        jsondata.geometry.coordinates[0]
+    ]); 
 
-    let marker = L.circleMarker([
-        47.267222, 11.392778
-    ]).bindPopup("Wettervorhersage").addTo(overlays.weather); 
+    let details = jsondata.properties.timeseries[0].data.instant.details; 
+    //console.log("Aktuelle Wetterdaten", details); 
+
+    let popup = `
+        <ul>
+            <li>Luftdruck: ${details.air_pressure_at_sea_level} (hPa)</li>
+            <li>Lufttemperatur: ${details.air_temperature} (°C)</li>
+            <li>Bewölkung: ${details.cloud_area_fraction} (%)</li>
+            <li>Niederschlag: ${details.precipitation_amount} (mm)</li>
+            <li>Relative Luftfeuchtigkeit: ${details.relative_humidity} (%)</li>
+            <li>Windrichtung: ${details.wind_from_direction} (°)</li>
+            <li>Windgeschwindigkeit: ${details.wind_speed* 3.6} (km/h)
+            </li>
+        </ul>
+        `;
+
+    marker.setPopupContent(popup).openPopup(); 
+
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
